42b3b52350c1028c9c537a36ef5777ba4ee86c9e
BrandonErb <brandonerb@gmail.com>
2019-08-19 19:16:04 -0400

    Finished Main page.

--- website/src/App.js
+++ website/src/App.js
@@ -6,8 +6,8 @@ import Main from './Main'
 
 function App() {
   return (
-    <div className="App">
-      <nav className="site-header sticky-top py-1">
+    <div className="App bgc">
+      <nav className="site-header sticky-top mx-auto">
         <div className="container d-flex flex-column flex-md-row justify-content-around">
           <a className="py-2 d-none d-md-inline-block" href="#">Home</a>
           <a className="py-2 d-none d-md-inline-block" href="#">About</a>
@@ -15,16 +15,15 @@ function App() {
         </div>
       </nav>
       <Main/>
-      <footer className="container py-5">
+      <footer className="container py-5 bgc">
         <div className="row">
-          <div className="col-12 col-md">
-            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx={12} cy={12} r={10} /><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" /></svg>
+          <div className="col-6 col-md">
             <small className="d-block mb-3 text-muted">© 2019</small>
           </div>
           <div className="col-6 col-md">
-            <h5>Contact</h5>
+            <h5 className="text-muted">Contact</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Email</a></li>
+              <li><a className="text-muted" href="#">service@galacticcommercecorportation.com</a></li>
             </ul>
           </div>
         </div>
--- website/src/Main.js
+++ website/src/Main.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import emblem from './images/GCC_Emblem.png'
+import backgroundVideo from './video/background_video.mp4'
 
 class Main extends React.Component{
   render() {
     return (
       <div>
-        <div className="position-relative overflow-hidden text-center main">
+        <div className="position-relative overflow-hidden text-center bgc">
           <div className="bg container-fluid">
             <div className="row">
               <div className="col-md-3 py-1 d-flex justify-content-end align-items-center">
@@ -18,8 +19,13 @@ class Main extends React.Component{
               </div>
             </div>
           </div>
-          <div className="col-md-5 p-lg-5 mx-auto my-5">
-            <h1 className="display-4 font-weight-normal">Welcome to the GCC</h1>
+          <div className="d-flex justify-content-center align-items-center container">
+            <video className="embed-responsive embed-responsive-16by9 video" autoPlay muted loop>
+              <source src={backgroundVideo} type="video/mp4" />
+            </video>
+            <div className="d-flex overlay">
+              <h1 className="display-2 font-weight-normal text-white text-outline">Welcome to the GCC</h1>
+            </div>
           </div>
         </div>
       </div>
