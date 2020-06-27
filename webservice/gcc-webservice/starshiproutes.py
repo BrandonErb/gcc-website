c8d25b8eb9c73d43335685b3d56a13222d683487
BrandonErb <brandonerb@gmail.com>
2020-06-26 20:14:59 -0400

    Working on Webservice
    Changing example code to starship app routes.

--- webservice/gcc-webservice/starshiproutes.py
+++ webservice/gcc-webservice/starshiproutes.py
@@ -1,59 +1,63 @@
 from app import app, mongo
-import bson 
+import bson
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify, request
-from werkzeug import generate_password_hash, check_password_hash
+#from werkzeug import generate_password_hash, check_password_hash
 
 @app.route('/ship/add', methods=['POST'])
 def add_ship():
-	_json = request.json
-	_name = _json['name']
-	_email = _json['email']
-	_password = _json['pwd']
-	# validate the received values
-	if _name and _email and _password and request.method == 'POST':
-		#do not save password as a plain text
-		_hashed_password = generate_password_hash(_password)
-		# save details
-		id = mongo.db.user.insert({'name': _name, 'email': _email, 'pwd': _hashed_password})
-		resp = jsonify('User added successfully!')
-		resp.status_code = 200
-		return resp
-	else:
-		return not_found()
+    _json = request.json
+    _id = _json['_id']
+    _name = _json['name']
+    _type = _json['type']
+    _image_url = _json['image_url']
+    _system = _json['system']
+    _address = _json['address']
+    _area = _json['area']
+    _ship_class = _json['class']
+    _slots = _json['slots']
+    # validate the received values
+    if _name and _email and _password and request.method == 'POST': 
+        # save details
+        mongo.db.user.insert({'name': _name, 'type': _name, 'image_url': _image_url, 'system': _system, 'address': _address, 'area': _area, 'class': _ship_class, 'slots': _slots})
+        resp = jsonify('Ship added successfully')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
 
-#Get		
+#Get        
 @app.route('/ships')
 def starships():
-	starships = mongo.db.starships.find()
-	resp = dumps(starships)
-	return resp
-	
+    starships = mongo.db.starships.find()
+    resp = dumps(starships)
+    return resp
+    
 #Get All
 @app.route('/ship/<id>')
 def starship(id):
-	starship = mongo.db.starships.find_one({'_id': ObjectId(id)})
-	resp = dumps(starship)
-	return resp
+    starship = mongo.db.starships.find_one({'_id': ObjectId(id)})
+    resp = dumps(starship)
+    return resp
 
 @app.route('/ship/update', methods=['PUT'])
 def update_starship():
-	_json = request.json
-	_id = _json['_id']
-	_name = _json['name']	
-	mongo.db.user.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': _name, 'email': _email, 'pwd': _hashed_password}})
-	resp = jsonify('User updated successfully!')
-	resp.status_code = 200
-	return resp
-		
+    _json = request.json
+    _id = _json['_id']
+    _name = _json['name']    
+    mongo.db.user.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': _name, 'email': _email, 'pwd': _hashed_password}})
+    resp = jsonify('Ship updated successfully')
+    resp.status_code = 200
+    return resp
+        
 @app.route('/ship/delete/<id>', methods=['DELETE'])
 def delete_ship(id):
-	mongo.db.user.delete_one({'_id': ObjectId(id)})
-	resp = jsonify('User deleted successfully!')
-	resp.status_code = 200
-	return resp
-		
+    mongo.db.user.delete_one({'_id': ObjectId(id)})
+    resp = jsonify('User deleted successfully!')
+    resp.status_code = 200
+    return resp
+        
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
