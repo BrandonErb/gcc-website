2b3cb78144c3a1e45ecc55634bb0b63a5ee86da8
BrandonErb <brandonerb@gmail.com>
2020-08-10 18:02:56 -0400

    FInished Post Method
    Finished and tested POST method for starships.

--- webservice/gcc-webservice/starshiproutes.py
+++ webservice/gcc-webservice/starshiproutes.py
@@ -3,12 +3,12 @@ import bson
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify, request
-#from werkzeug import generate_password_hash, check_password_hash
 
+#Post add new ship
 @app.route('/ship/add', methods=['POST'])
 def add_ship():
     _json = request.json
-    _id = _json['_id']
+    #_id = _json['_id']
     _name = _json['name']
     _type = _json['type']
     _image_url = _json['image_url']
@@ -18,23 +18,23 @@ def add_ship():
     _ship_class = _json['class']
     _slots = _json['slots']
     # validate the received values
-    if _name and _email and _password and request.method == 'POST': 
+    if request.method == 'POST': 
         # save details
-        mongo.db.user.insert({'name': _name, 'type': _name, 'image_url': _image_url, 'system': _system, 'address': _address, 'area': _area, 'class': _ship_class, 'slots': _slots})
+        mongo.db.starships.insert({'name': _name, 'type': _type, 'image_url': _image_url, 'system': _system, 'address': _address, 'area': _area, 'class': _ship_class, 'slots': _slots})
         resp = jsonify('Ship added successfully')
         resp.status_code = 200
         return resp
     else:
         return not_found()
 
-#Get        
+#Get All    
 @app.route('/ships')
 def starships():
     starships = mongo.db.starships.find()
     resp = dumps(starships)
     return resp
     
-#Get All
+#Get One
 @app.route('/ship/<id>')
 def starship(id):
     starship = mongo.db.starships.find_one({'_id': ObjectId(id)})
@@ -44,9 +44,15 @@ def starship(id):
 @app.route('/ship/update', methods=['PUT'])
 def update_starship():
     _json = request.json
-    _id = _json['_id']
-    _name = _json['name']    
-    mongo.db.user.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': _name, 'email': _email, 'pwd': _hashed_password}})
+    _name = _json['name']
+    _type = _json['type']
+    _image_url = _json['image_url']
+    _system = _json['system']
+    _address = _json['address']
+    _area = _json['area']
+    _ship_class = _json['class']
+    _slots = _json['slots']
+    mongo.db.starships.update_one({'name': _name}, {'$set': {'type': _type, 'image_url': _image_url, 'system': _system, 'address': _address, 'area': _area, 'class': _ship_class, 'slots': _slots}})
     resp = jsonify('Ship updated successfully')
     resp.status_code = 200
     return resp
